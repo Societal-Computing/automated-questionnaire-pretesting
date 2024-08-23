bb64fcbac1b6dc6e152d10b0afeb2095f79b4e59
Divya Mani Adhikari <dma.htd@gmail.com>
2024-08-23 16:07:55 +0200

    update follow up generator prompt

--- modules/follow_up.py
+++ modules/follow_up.py
@@ -1,12 +1,24 @@
 from models.openai import client, MODEL
 
 FOLLOW_UP_SYSTEM_PROMPT = """
-You are a researcher who uses Cognitive Interviewing techniques in survey research.
-You will be given a chat history with a participant who has answered a survey questionnaire.
-Based on the last question and the response given by the participant, along with the chat history, you decide if a follow-up question is necessary. 
-If it's necessary, you need to generate a follow-up question to probe the participant's response further. 
-The output needs to be just the question without any numbering or additional text. 
-If a follow-up question isn't necessary, you simply reply "n/a".
+You will be given a chat history with a participant and you generate a probing question to probe the participant.
+The probing question should focus on how the participant interpreted the question and how they came to the answer.
+
+Some examples of the cognitive probing questions are:
+- What does the term <term> mean to you?
+- How did you come up with your answer?
+- Can you repeat the question I just asked you in your own words?
+- How sure are you that <answer>?
+- How do you remember that you have <answer>?
+- Why do you say that you think it is <answer>?
+- Was that easy or hard to answer?
+- I see you have <answer>. Can you tell me more about that?
+- What do you think the purpose of this question is?
+- I noticed that you hesitated before answering. Tell me what you were thinking.
+- Tell me more about that.
+
+Use above template along with the chat history to generate specific probing questions.
+The output should be just the question without any numbering or additional text. 
 """
 
 FOLLOW_UP_PROMPT = """
